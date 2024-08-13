d0a81406e79e93aa982fb237a768a75d1a7fec88
yassineraddaoui <yassineraddaouicontact@gmail.com>
2024-08-13 16:42:54 +0100

    fix async communication

--- Message/src/main/java/com/spring/async/message/Functions.java
+++ Message/src/main/java/com/spring/async/message/Functions.java
@@ -5,7 +5,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.function.BiFunction;
+import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.function.UnaryOperator;
 
 @Configuration
 public class Functions {
@@ -21,6 +24,13 @@ public class Functions {
         return inputDTO -> {
             log.info("Function Two processed");
             return inputDTO.message();
+        };
+    }
+    @Bean
+    public Function<String,String> print(){
+        return (s)-> {
+            log.info("Function Three processed{}", s);
+            return s;
         };
     }
 }
--- applicationA/src/main/java/com/spring/async/applicationa/ApplicationAApplication.java
+++ applicationA/src/main/java/com/spring/async/applicationa/ApplicationAApplication.java
@@ -7,22 +7,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.stream.function.StreamBridge;
 
+import java.util.Collections;
 import java.util.logging.Logger;
 
 @SpringBootApplication
 @AllArgsConstructor
 public class ApplicationAApplication implements CommandLineRunner {
-    private static final Logger log = Logger.getLogger(Caller.class.getName());
+    private static final Logger log = Logger.getLogger(ApplicationAApplication.class.getName());
     private final StreamBridge streamBridge;
 
     public static void main(String[] args) {
-        SpringApplication.run(ApplicationAApplication.class, args);
-    }
+        var app = new SpringApplication(ApplicationAApplication.class);
+        app.run(args);
 
-    CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-        return args -> {
-
-        };
     }
 
     @Override
